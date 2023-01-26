9d599e4edc8589e0efe66c98f114f9ce3d6d5c50
Paul De Palma <depalma@gonzaga.edu>
2023-01-25 18:32:31 -0800

    save

---
+++ 2-Intro/6-caesar/pythonVersion/caesar.py
@@ -0,0 +1,16 @@
+import random
+def key_gen():
+    return random.randint(1,25)    
+
+def encrypt(ch,key):
+    pos = ord(ch) - ord('A')
+    pos = (pos + key) % 26
+    pos = pos + ord('A')
+    return chr(pos)
+
+def decrypt(ch,key):
+    pos = ord(ch) - ord('A')
+    pos = (pos- key) % 26  
+    pos = pos + ord('A')
+    return chr(pos)
+
